21fae5dacc802036051be3b1fb13c05f6e7e5c89
Peter Thompson <PNT510@gmail.com>
2024-09-20 11:16:11 -0400

    Fetch implemented

--- server/server.js
+++ server/server.js
@@ -11,6 +11,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extend: false}));
 
+//CONTROLLER
+const booksController = require('./controllers/books_controller');
+app.use('/api/books', booksController);
+
 //LISTEN
 app.listen(4005, () => {
     console.log('Server is running on port 4005');
